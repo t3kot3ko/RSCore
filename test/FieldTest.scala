d8812d26847832855d2e43687c73c6dd71a64caa
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2012-10-20 15:17:38 +0900

    Add, Settings: test fundation was created

---
+++ test/FieldTest.scala
@@ -0,0 +1,18 @@
+package tests.dsl
+import org.eclipse.ui.PlatformUI
+import org.eclipse.ui.IWorkbench
+import org.junit.Test
+import org.junit.Assert._
+
+
+class FieldTest{
+	@Test
+	def testSample(): Unit = {
+		println("##############################################################")
+		println("##############################################################")
+		println("##############################################################")
+		println("##############################################################")
+		println("##############################################################")
+		assertTrue(true)
+	}
+}
