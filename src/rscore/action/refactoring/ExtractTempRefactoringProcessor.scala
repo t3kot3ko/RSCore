f9aba479c4c10354943f325a068d850e8d3b16fb
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2012-12-07 14:41:00 +0900

    Modify: ExtractTempRefactoringProcessor

--- src/rscore/action/refactoring/ExtractTempRefactoringProcessor.scala
+++ src/rscore/action/refactoring/ExtractTempRefactoringProcessor.scala
@@ -1,7 +1,12 @@
 package rscore.action.refactoring
+import rscore.dsl.detail.expression.RSExpression
+import org.eclipse.jdt.internal.corext.refactoring.code.ExtractTempRefactoring
 
-class ExtractTempRefactoringProcessor extends AbstractRefactoringProcessor{
-	override def createAction: RSRefactoringAction = {
+class ExtractTempRefactoringProcessor(expression: RSExpression) extends AbstractRefactoringProcessor{
+	override def createAction(): RSRefactoringAction = {
+		val cu = expression.origin.
+		val ref = new ExtractTempRefactoring()
+		
 		return null
 	}
 
